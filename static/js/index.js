673898c6991fe3383e14677ab8e4f6fdd4664d29
Paul Maldonado <maldonadopaul92@gmail.com>
2021-04-14 00:09:54 -0500

    sensor1

--- static/js/index.js
+++ static/js/index.js
@@ -55,8 +55,10 @@
   // visualizacion de los estados del led
   
 function onMessageArrived(message) {
-	 texto=(message.payloadString);
-	 document.getElementById("estado").innerHTML=texto;
+	 texto=(message.payloadString)split(":")[0];
+	 texto2=(message.payloadString)split(":")[1];
+	 document.getElementById("estado1").innerHTML=texto;
+	 document.getElementById("estado2").innerHTML=texto2;
 }
 
 
